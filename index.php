d837141e37b599ff042ce2df385aa3bedc331d0f
shookh-111 <90shohjahon@gmail.com>
2025-11-29 15:52:33 +0500

    while sikili

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
-<?php
 
-$a = 66;
-
-if ($a > 0 && $a < 10) {
-
-    echo $a ** 3;
-
-} elseif ($a > 10 && $a <= 20) {
-
-    echo $a ** 3;
-
-} elseif ($a > 20) {
+    <?php
+$a =0;
+ while ($a<20) {
+    $a+=2;
+  echo $a;
    
-    echo sqrt($a);
-
-} else {
-    echo "Bunday son kiritish mumkin emas!";
-}
-
+ }
 ?>
+
+
