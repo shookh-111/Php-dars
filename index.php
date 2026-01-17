930e0e30ea81ff81552eefb9dfd90405fe97b63e
shookh-111 <90shohjahon@gmail.com>
2026-01-17 17:24:37 +0500

    1

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
-$son = readline("Son kiriting: ");
-
-$tub = true;
-
-if ($son < 2) {
-    $tub = false;
-}
-
-for ($i = 2; $i < $son; $i++) {
-    if ($son % $i == 0) {
-        $tub = false;
-        break;
-    }
-}
-
-if ($tub) {
-    echo "$son - tub son.";
-} else {
-    echo "$son - tub son emas.";
+$i = 100;
+while ($i >= 1) {
+echo $i . " ";
+$i--;
 }
 ?>
